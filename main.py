1e85a63d944758035d84f5964376edceb0ff7fb6
un4rch <81522320+un4rch@users.noreply.github.com>
2023-11-18 19:58:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 # Visualizacion de embeddings: https://projector.tensorflow.org/
-"""
-import warnings
-warnings.filterwarnings('ignore')
+# TODO al guardar los embeddings, verificar que se guardan bien, ya que con to_csv se guardan distinto
+# TODO multinomialNB da error con embeddings por contener valores negativos, ver si se puede arreglar y sino, pues no usarlo
 
-import numpy as np # linear algebra
-
-# Visualisation libraries
-import matplotlib.pyplot as plt
+# Algoritmos: 
+#import ast
+# Warnings
+#import warnings
+#warnings.filterwarnings('ignore')
+# System libraries
+import os
+import sys
+import csv
+# Visualization libraries
 import seaborn as sns
-
-# Machine Learning Libraries
-from sklearn.model_selection import train_test_split,GridSearchCV,RandomizedSearchCV
-from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+# Data processing libraries
+import pandas as pd # frames (tables)
+import numpy as np # linear algebra
+#import string
+# NLP preprocessing libraries
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from preprocessor import Preprocessor
+#from sklearn.feature_extraction.text import TfidfVectorizer
+# Data serialization 
+import pickle
+# Classification algorithms
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV # Search best classifier for hyperparameter combinations
 from sklearn.feature_selection import SelectKBest,chi2,f_classif
 from sklearn.ensemble import RandomForestClassifier,VotingClassifier,AdaBoostClassifier,GradientBoostingClassifier,BaggingClassifier
 from sklearn.metrics import classification_report , confusion_matrix
@@ -19,79 +37,67 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+# Evaluation metrics
+import evaluate
+from sklearn import metrics
 
-import pickle
-import string
-"""
-# System libraries
-import os
-import sys
-# Visualization libraries
-import seaborn as sns
-import matplotlib.pyplot as plt
-# Data processing libraries
-import pandas as pd # frames (tables)
-# NLP preprocessing libraries
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from preprocessor import Preprocessor
-
-#preprocessedFile = "Preprocessed_Suicide_Detection.csv"
-preprocessedFile = None
+preprocessedFile = "Preprocessed_Suicide_Detection.csv"
 unpreprocessedFile = "Suicide_Detection.csv"
-savePreprocess = "Preprocessed_Suicide_Detection.csv"
+guardarPreproceso = "Preprocessed_Suicide_Detection.csv"
 output_dir = "output"
+train = True
+visualization = False
 textLengthsFilter = 10000 # Se eliminan los textos cuya longitud sea mas de 10000 caracteres
 histogramIntervals = 500
+pca_dimensions = 200
+doc2vec_vectors_size = 1500
+doc2vec_model = None
+pca_model = None
 
 def cargarDataset(pFileName):
     # Cargar los datos
-    data = pd.read_csv(pFileName)
-    # Obtener 10000 instancias
-    df = data.sample(n=10000, random_state=42)
-    # Mostrar la informacion del dataframe y eliminar la columna que no nos interesa
-    #print(df.info())
-    df.drop(columns = 'Unnamed: 0',inplace=True)
+    df_tmp = pd.read_csv(pFileName)
+    # Coger solo las columnas que nos interesan
+    df = pd.DataFrame()
+    df['text'] = df_tmp['text']
+    df['class'] = df_tmp['class']
     return df
 
-def preprocesado(dataFrame):
-    prerocessor = Preprocessor()
-    for row in dataFrame['text'].values:
-        print("----------------------------------------")
-        print(row)
-        linea = prerocessor.preprocesarLenguajeNatural(row, True)
-        print(linea)
-    print(linea)
-    sys.exit(0)
-    """# Convertir a minusculas
-    dataFrame['text']= dataFrame['text'].str.lower()
-    # Eliminar puntuaciones
-    dataFrame['text'] = dataFrame['text'].str.replace(r'[^\w\s]+', '',regex = True)
-    stop_words = stopwords.words('english')
-    dataFrame['text'] = dataFrame['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
-    # Tokenizar
-    dataFrame['text'] = dataFrame['text'].apply(lambda x:nltk.word_tokenize(x))
-    # Lematizar palabras (stemming)
-    ps = PorterStemmer()
-    dataFrame['text'] = dataFrame['text'].apply(lambda x : [ps.stem(i) for i in x])
-    dataFrame['text']=dataFrame['text'].apply(lambda x : ' '.join(x))"""
-    # Eliminar si hay algun valor vacio
-    #ind = dataFrame[dataFrame['text'].isnull()].index # indice de instancias vacias
-    #print(dataFrame.iloc[ind]) # imprimir contenido de las instancias vacias
-    dataFrame.dropna(inplace=True)
-    # PCA space dim reduction
-    # TODO
-    # Document embeddings
-    # TODO
+def preprocesado(dataFrame, doc2vec_model, pca_model):
+    # Obtener 10000 instancias
+    dataFrame = dataFrame.sample(n=10000, random_state=42)
+    # Filtrar textos por longitud usando un umbral
+    dataFrame = dataFrame[dataFrame['text_length']<=textLengthsFilter]
+    # Preprocesamos los datos
+    preprocessor = Preprocessor()
+    x_prep,y_prep,doc2vec_model,pca_model = preprocessor.doc2vec(dataFrame['text'], dataFrame['class'], pca_dimensions=pca_dimensions, doc2vec_vectors_size=doc2vec_vectors_size, doc2vec_model=doc2vec_model, pca_model=pca_model)
+    # Convertir de np.array a tuplas
+    x_prep = [tuple(point) for point in x_prep.tolist()]
     # Guardar el preproceso
-    dataFrame.to_csv("Preprocessed_Suicide_Detection.csv")
+    if guardarPreproceso != None:
+            #dataFrame.to_csv(guardarPreproceso) #¿¿¿por que al guardar asi se guardan distinto???
+            # Guardar datos preprocesados
+            with open(guardarPreproceso, "w") as file:
+                writer = csv.writer(file)
+                writer.writerow(["text","class"])
+                for idx,point in enumerate(x_prep):
+                    writer.writerow([point,y_prep[idx]])
+            print(f"    Fichero guardado: {guardarPreproceso}")
+            if train:
+                # Guardar los modelos de d0c2vec y pca
+                doc2vec_model.save(unpreprocessedFile.split(".")[0]+"_doc2vec.model")
+                with open(unpreprocessedFile.split(".")[0]+"_pca.model", "wb") as file:
+                    pickle.dump(pca_model, file)
+                print(f"    Fichero guardado: {unpreprocessedFile.split('.')[0]+'_doc2vec.model'}")
+                print(f"    Fichero guardado: {unpreprocessedFile.split('.')[0]+'_pca.model'}")
+    # Devolvemos el dataFrame preprocesado
+    dataFrame['text'] = x_prep
+    dataFrame['class'] = y_prep
     return dataFrame
 
 def histogramTextsLengths(dataFrame, fileName):
-    dataFrame['text_lengths'] = dataFrame['text'].apply(len)
     # Create a histogram with intervals of 500
-    plt.hist(dataFrame['text_lengths'], bins=range(0, max(dataFrame['text_lengths']) + histogramIntervals + 1, histogramIntervals), edgecolor='black')
+    plt.hist(dataFrame['text_length'], bins=range(0, max(dataFrame['text_length']) + histogramIntervals + 1, histogramIntervals), edgecolor='black')
 
     # Set labels and title
     plt.xlabel('Text Length')
@@ -101,36 +107,17 @@ def histogramTextsLengths(dataFrame, fileName):
     # Save the plot as a PNG file
     plt.savefig(f"{output_dir}/{fileName}")
     plt.close()
+    print(f"    Fichero guardado: {output_dir}/{fileName}")
 
 def boxplotTextLengths(dataFrame, fileName):
-    # Function to calculate length of each item
-    def calculate_length(item):
-        return len(str(item))
-    dataFrame['text_lengths'] = dataFrame['text'].apply(calculate_length)
-    #print(dataFrame['text_lengths'].sum())
-    sns.boxplot(x="text_lengths", y="class", data=dataFrame)
+    sns.boxplot(x="text_length", y="class", data=dataFrame)
     plt.savefig(f"{output_dir}/class_separated_{fileName}")
     plt.close()
-    sns.boxplot(x="text_lengths", data=dataFrame)
+    print(f"    Fichero guardado: {output_dir}/class_separated_{fileName}")
+    sns.boxplot(x="text_length", data=dataFrame)
     plt.savefig(f"{output_dir}/{fileName}")
     plt.close()
-
-    # resetear indices
-    #textLenghts = textLenghts.reset_index(drop=True)
-    """
-    textLenghts = []
-    for row in df.values:
-        # Access row data using row[index]
-        print(row[0])
-        print(row[1])
-        print("-----------")
-    for idx,row in dataFrame.iterrows():
-        text = row['text']
-        label = row['class']
-        textLenghts.append(len(text))
-    #sns.boxplot()
-    print(sum(textLenghts))
-    """
+    print(f"    Fichero guardado: {output_dir}/{fileName}")
 
 def visualizeBalanced(dataFrame, fileName):
     classCnt = df['class'].value_counts()
@@ -146,28 +133,169 @@ def visualizeBalanced(dataFrame, fileName):
 
     plt.savefig(f"{output_dir}/{fileName}")
     plt.close()
-    
+    print(f"    Fichero guardado: {output_dir}/{fileName}")
+
+def saveConfussionMatrix(confussion_matrix, dType, fileName, cmap):
+    plt.figure()
+    sns.heatmap(confussion_matrix, annot=True, fmt=dType, cmap=cmap)
+    plt.title(fileName.split(".")[0])
+    #plt.ylabel("Clusters")
+    #plt.xlabel("Clases")
+    plt.savefig(f"{output_dir}/{fileName}", format='png')
+    plt.close()
+    print(f"    Fichero guardado: {output_dir}/{fileName}")
+
 if __name__ == "__main__":
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    if not preprocessedFile:
-        df = cargarDataset(unpreprocessedFile)
-        df = preprocesado(df)
-    else:
-        df = cargarDataset(preprocessedFile)
+    # Cargar dataset sin preprocesar
+    df = cargarDataset(unpreprocessedFile)
+    # Calcular una columna que contenga las longitudes de los textos (sirve para el preprocesado)
+    df['text_length'] = df['text'].apply(len)
     # Eliminar si ha quedado algun valor vacio al cargar el dataset
     df.dropna(inplace=True)
-    # Visualizar si los datos estan balanceados
-    visualizeBalanced(df, "check_balanced.png")
-    # Generar un histograma que muestra en numero de instancias que tienen cierta cantidad de letras por intervalos
-    histogramTextsLengths(df, "text_lengths_histogram_afer_before.png")
-    # Generar un boxplot que represente las longitudes de los datos de entrada
-    boxplotTextLengths(df, "text_lenghts_boxplot_before_cleaning.png")
-    # Filtrar textos por longitud usando un umbral
-    df = df[df['text_lengths']<=textLengthsFilter]
-    # Volver a generar un histograma con el filtrado hecho
-    histogramTextsLengths(df, "text_lengths_histogram_afer_cleaning.png")
-    # Volver a generar un boxplot con el filtrado hecho
-    boxplotTextLengths(df, "text_lenghts_boxplot_after_cleaning.png")
-    # Coger los textos y las etiquetas
-    x,y = df['text'],df['class']
+    # Crear directorio donde se van a guardar las graficas y ficheros generados
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    if visualization:
+        print("[*] Generando graficas de visualizacion de los datos...")
+        # Visualizar si los datos estan balanceados
+        visualizeBalanced(df, "check_balanced.png")
+        # Generar un histograma que muestra en numero de instancias que tienen cierta cantidad de letras por intervalos
+        histogramTextsLengths(df, "text_lengths_histogram_afer_before.png")
+        # Generar un boxplot que represente las longitudes de los datos de entrada
+        boxplotTextLengths(df, "text_lengths_boxplot_before_cleaning.png")
+        # Filtrar textos por longitud usando un umbral
+        df = df[df['text_length']<=textLengthsFilter]
+        # Volver a generar un histograma con el filtrado hecho
+        histogramTextsLengths(df, "text_lengths_histogram_afer_cleaning.png")
+        # Volver a generar un boxplot con el filtrado hecho
+        boxplotTextLengths(df, "text_lengths_boxplot_after_cleaning.png")
+        # Coger los textos y las etiquetas
+    # Preprocesar datos
+    if not preprocessedFile:
+        print("[*] Preprocesando datos...")
+        df = preprocesado(df, doc2vec_model, pca_model)
+    else:
+        df = cargarDataset(preprocessedFile)
+        # Eliminar \n para cargar los embeddings si los hay
+        df['text'] = df['text'].replace('\n', '', regex=True)
+    
+    # Cargar los datos preprocesados en arrays
+    embeddings = [eval(point) for point in df['text']]
+    labels = np.asarray(df['class'])
+
+    print("[*] Entrenando modelos...")        
+    # Separar dataset en entrenamiento y pruebas
+    X_train,X_test,y_train,y_test = train_test_split(embeddings,labels,test_size=0.2,random_state=42)
+
+    # Clasificacion GaussianNB
+    print(f"\t\t\tGaussianNB")
+    print(f"{24*'-'}--------------{24*'-'}")
+    gnb = GaussianNB()
+    gnb.fit(X_train, y_train)
+    y_pred = gnb.predict(X_test)
+    cm = confusion_matrix(y_test,y_pred)
+    print(classification_report(y_test,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_gaussianNB.png", cmap='summer')
+    print()
+
+    """# Clasificacion MultinomialNB
+    mnb = MultinomialNB()
+    mnb.fit(X_train, y_train)
+    y_pred = mnb.predict(X_test)
+    cm = confusion_matrix(y_test,y_pred)
+    print(classification_report(y_test,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_multinomialNB.png")"""
+    
+    # Clasificacion BernoulliNB
+    print(f"\t\t\tBernoulliNB")
+    print(f"{24*'-'}--------------{24*'-'}")
+    bnb = BernoulliNB()
+    bnb.fit(X_train, y_train)
+    y_pred = bnb.predict(X_test)
+    cm = confusion_matrix(y_test,y_pred)
+    print(classification_report(y_test,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_binomialNB.png", cmap='summer')
+    print()
+
+    # Clsificacion Random Forest
+    print(f"\t\t\tRandomForest")
+    print(f"{24*'-'}--------------{24*'-'}")
+    rfc = RandomizedSearchCV(RandomForestClassifier(),{'n_estimators':[4,5],'criterion':['entropy'],
+                                                      'max_depth':range(1,4),'min_samples_split':range(2,5)},random_state=12)
+    rfc.fit(X_train, y_train)
+    #print('Training score:',rfc.score(X_train, y_train))
+    #print('Testing score:',rfc.score(X_test,y_test))
+    print(rfc.best_estimator_)
+    y_act=y_test
+    y_pred=rfc.predict(X_test)
+    cm = confusion_matrix(y_act,y_pred)
+    print(classification_report(y_act,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_RandomForest.png", cmap='Spectral')
+    print()
+
+    # Clsificacion Decision Tree
+    print(f"\t\t\tDecisionTree")
+    print(f"{24*'-'}--------------{24*'-'}")
+    dtc= DecisionTreeClassifier(criterion='gini',splitter='random',min_samples_leaf=70,max_depth=4,random_state=0)
+    dtc.fit(X_train, y_train)
+    #print(model2.score(X_train, y_train))
+    #print(model2.score(X_test,y_test))
+    y_act=y_test
+    y_pred=dtc.predict(X_test)
+    cm = confusion_matrix(y_act,y_pred)
+    print(classification_report(y_act,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_DecisionTree.png", cmap='PiYG')
+    print()
+
+    # TODO Meter mas modelos de prediccion
+
+    # Clasificacion usando Ensemble Methods
+    print(f"\t\t\tEnsembleMethods")
+    print(f"{24*'-'}--------------{24*'-'}")
+    ensemble_model = VotingClassifier(
+        estimators=[("GaussianNB", GaussianNB()),
+                    ("BernoulliNB", BernoulliNB()),
+                    ("RandomForestClassifier", RandomForestClassifier()),
+                    ("DecisionTreeClassifier", DecisionTreeClassifier())],
+        voting='soft'  # 'hard' for majority voting, 'soft' for weighted voting based on class probabilities
+    )
+    # Fit the ensemble model on the training data
+    ensemble_model.fit(X_train, y_train)
+    # Make predictions on the test set
+    y_pred = ensemble_model.predict(X_test)
+    cm = confusion_matrix(y_act,y_pred)
+    print(classification_report(y_act,y_pred))
+    saveConfussionMatrix(cm, "d", "confussion_matrix_EnsembleMethods.png", cmap='PiYG')
+    print()
+
+    """
+    # Buscar el modelo con mejores hyperparametros:
+    # Create a Random Forest Classifier
+    rf_classifier = RandomForestClassifier()
+
+    # Define the hyperparameter grid
+    param_dist = {
+        'n_estimators': randint(10, 200),
+        'max_depth': randint(1, 20),
+        'min_samples_split': randint(2, 20),
+        'min_samples_leaf': randint(1, 20),
+        'bootstrap': [True, False]
+    }
+
+    # Create the RandomizedSearchCV object
+    random_search = RandomizedSearchCV(rf_classifier, param_distributions=param_dist, n_iter=10, cv=5, scoring='accuracy', random_state=42)
+
+    # Fit the model
+    random_search.fit(X_train, y_train)
+
+    # Print the best hyperparameters
+    print("Best Hyperparameters:", random_search.best_params_)
+
+    # Get the best model
+    best_model = random_search.best_estimator_
+
+    # Evaluate the best model on the test set
+    y_pred = best_model.predict(X_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    print(f"Accuracy of the Best Model: {accuracy}")
+    """
